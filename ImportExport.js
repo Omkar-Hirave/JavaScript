202bb70808140912e8978f5e3212fb78d7739a53
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-08-23 07:15:10 +0530

    Add files via upload

---
+++ ImportExport.js
@@ -0,0 +1,14 @@
+import {mul} from "./utility.js"
+import { add } from "./utility.js";
+// import * as all from "./utility.js"
+// import substract  from "./utility.js";
+
+
+const result = mul(10 , 20 , 30 );
+console.log("Multiplication is --> ",result);
+
+const res = add(10, 20 , 30);
+console.log("Addition is --> ",res);
+
+const sub = substract(10, 20 , 30);
+console.log("substraction  is --> ",sub);
