1f2dabba0c5f971df141caa1732a9bf6d7096672
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-08-23 07:16:13 +0530

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,16 @@
+ function multiply(a, b, c) {
+    return a * b * c;
+}
+
+ function default  addition(a, b, c) {
+    return a + b + c;
+}
+
+export  (a, b, c)=> {
+    return a - b - c;
+}
+
+export {
+    multiply as mul ,
+    addition as add
+}
